c2139cc94c7e6189e140ba77e913f012f87b47fc
AtharvNadkarni2013 <81621785+AtharvNadkarni2013@users.noreply.github.com>
2024-05-27 13:47:47 +0530

    code

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function (req, res) {
+  const ipaddress = req.ip;
+  const { "accept-language": language, 'user-agent': software } = req.headers
+  res.json({ ipaddress, language, software })
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
